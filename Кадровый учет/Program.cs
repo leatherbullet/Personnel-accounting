f90c873b65ae7d53a87235c29402d30c39bc4ae1
leatherbullet <143252652+leatherbullet@users.noreply.github.com>
2024-03-29 00:14:04 +0300

    Update Program.cs

--- Кадровый учет/Program.cs
+++ Кадровый учет/Program.cs
@@ -10,62 +10,45 @@ namespace Кадровый_учет
     {
         static void Main(string[] args)
         {
-            const string CommandToAddDossier = "1";
-            const string CommandToShowAllDossier = "2";
-            const string CommandToDeliteDossier = "3";
-            const string CommandToSearchByLastName = "4";
-            const string CommandToExit = "5";
+            const string CommandAdd = "1";
+            const string CommandShowAll = "2";
+            const string CommandDelite = "3";
+            const string CommandSearch = "4";
+            const string CommandExit = "5";
 
-            string[] namesDataBase = new string[0];
-            string[] jobDataBase = new string[0];
+            string[] employeesNames = new string[0];
+            string[] employeesPosition = new string[0];
             string userInput;
             bool isWork = true;
 
             while (isWork)
             {
-                Console.WriteLine($"{CommandToAddDossier}: добавить досье");
-                Console.WriteLine($"{CommandToShowAllDossier}: вывести все досье");
-                Console.WriteLine($"{CommandToDeliteDossier}: удалить досье");
-                Console.WriteLine($"{CommandToSearchByLastName}: поиск по фамилии");
-                Console.WriteLine($"{CommandToExit}: выход");
+                Console.WriteLine($"{CommandAdd}: добавить досье");
+                Console.WriteLine($"{CommandShowAll}: вывести все досье");
+                Console.WriteLine($"{CommandDelite}: удалить досье");
+                Console.WriteLine($"{CommandSearch}: поиск по фамилии");
+                Console.WriteLine($"{CommandExit}: выход");
                 userInput = Console.ReadLine();
 
                 switch (userInput)
                 {
-                    case "1":
-                        Console.WriteLine("введите фамилию");
-                        userInput = Console.ReadLine();
-
-                        AddDossier(ref namesDataBase, userInput);
-
-                        Console.WriteLine("введите должность");
-                        userInput = Console.ReadLine();
-
-                        AddDossier(ref jobDataBase, userInput);
+                    case CommandAdd:
+                        AddDossier(ref employeesNames, ref employeesPosition, userInput);
                         break;
 
-                    case "2":
-                        ShowAllDossier(namesDataBase, jobDataBase);
+                    case CommandShowAll:
+                        ShowAllEmployees(employeesNames, employeesPosition);
                         break;
 
-                    case "3":
-                        ShowAllDossier(namesDataBase, jobDataBase);
-
-                        Console.Write("удаление сотрудника из базы данных. выберете номер:");
-                        userInput = Console.ReadLine();
-
-                        DeliteDossier(ref namesDataBase, userInput);
-                        DeliteDossier(ref jobDataBase, userInput);
+                    case CommandDelite:
+                        DeliteEmployee(ref employeesNames, ref employeesPosition, userInput);
                         break;
 
-                    case "4":
-                        Console.WriteLine("Введите фамилию: ");
-                        userInput = Console.ReadLine();
-
-                        SearchSurname(namesDataBase, jobDataBase, userInput);
+                    case CommandSearch:
+                        SearchSurname(employeesNames, employeesPosition, userInput);
                         break;
 
-                    case "5":
+                    case CommandExit:
                         isWork = false;
                         break;
 
@@ -79,45 +62,81 @@ namespace Кадровый_учет
             }
         }
 
-        static void AddDossier(ref string[] dataBase, string input)
+        static void AddDossier(ref string[] dataBase, ref string[] position, string input)
         {
-            string[] tempDataBase = new string[dataBase.Length + 1];
+            Console.WriteLine("введите Ф.И.О.");
+            input = Console.ReadLine();
+            
+            Console.WriteLine("введите должность");
+            input = Console.ReadLine();
 
-            for (int i = 0; i < dataBase.Length; i++)
-                tempDataBase[i] = dataBase[i];
-
-            tempDataBase[tempDataBase.Length - 1] = input;
-            dataBase = tempDataBase;
+            AddEmployee(ref namesData, input);
+            AddEmployee(ref position, input)
         }
 
-        static void ShowAllDossier(string[] nameBase, string[] jobBase)
+        static void AddEmployee(ref string[] data, string input)
+        {
+            string[] tempData = new string[data.Length + 1];
+
+            for (int i = 0; i < data.Length; i++)
+                    tempData[i] = data[i];
+
+            tempData[tempData.Length - 1] = input;
+            data = tempData;
+        }
+
+        static void ShowAllEmployees(string[] namesData, string[] position)
         {
             Console.WriteLine("список сотрудников:");
 
-            for (int i = 0; i < nameBase.Length; i++)
-                Console.WriteLine($"{i + 1}: {nameBase[i]} - {jobBase[i]}");
+            for (int i = 0; i < namesData.Length; i++)
+                Console.WriteLine($"{i + 1}: {namesData[i]} - {position[i]}");
         }
 
-        static void DeliteDossier(ref string[] dataBase, string input)
+        static void DeliteEmployee(ref string[] namesData, ref string[] position, string input)
         {
-            string[] tempBase = new string[dataBase.Length - 1];
+             ShowAllEmployees(namesData, position);
 
+             Console.Write("удаление сотрудника из базы данных. выберете номер:");
+             input = Console.ReadLine();
+
+             RemoveEmployee(ref namesData, input);
+             RemoveEmployee(ref position, input);
+        }
+
+        static void RemoveEmployee(ref string[] data, string input)
+        {
             if (int.TryParse(input, out int index))
+                if (index > data.Length || index <= 0)
+                   Console.WriteLine("неправильный ввод");
+               else
+              {
+                string[] tempBase = new string[data.Length - 1];
 
-            for (int i = 0; i < index - 1; i++)
-                    tempBase[i] = dataBase[i];
+                for (int i = 0; i < index - 1; i++)
+                       tempBase[i] = data[i];
 
-            for (int i = index; i < dataBase.Length; i++)
-                tempBase[i - 1] = dataBase[i];
+                for (int i = index; i < data.Length; i++)
+                tempBase[i - 1] = data[i];
 
-            dataBase = tempBase;
+                data = tempBase;
+               }
         }
-
-        static void SearchSurname(string[] nameBase, string[] jobBase, string input)
+        
+        static void SearchSurname(string[] namesData, string[] position, string input)
         {
-            for (int i = 0; i < nameBase.Length; i++)
-                if(input.ToLower() == nameBase[i].ToLower())
-                    Console.WriteLine($"сотрудник {i + 1}: {nameBase[i]} - {jobBase[i]}");
+            Console.Write("Введите фамилию: ");
+            input = Console.ReadLine();
+
+            char empty = ' ';
+            
+            for (int i = 0; i < namesData.Length; i++)
+            {
+                string[] name = namesData[i].Split(empty);
+
+                if (name[0] == input)
+                    Console.WriteLine($"сотрудник {i + 1}: {namesData[i]} - {position[i]}");
+            }
         }
     }
 }
